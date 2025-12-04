d94fa1e1f2293563898a735d96762ad4441251c9
Adlymusyaffatibr <musyaffatibradly@gmail.com>
2025-12-04 10:21:33 +0700

    Explore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:projek_jurnalku_to_mobile/profile.dart';
+import 'package:projek_jurnalku_to_mobile/explore.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: profile(),
+      home: Explore(),
     );
   }
 }
